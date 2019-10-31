fd3987de660051a2d5115dc8eda22143b9432977
Sean Hellum <seanhellum45@gmail.com>
2019-10-31 02:53:21 +0000

    update model

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import numpy as np
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
-model.fit(xs, ys, epochs=4000)
-print(model.predict([200]))
+def mathRelationshipID(x_data, y_data, number,epochsNum):
+    model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    xs = np.array(x_data, dtype=float)
+    ys = np.array(y_data, dtype=float)
+    model.fit(x_data, y_data, epochs=epochsNum)
+    model.predict(number)
+mathRelationshipID([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0],[-2.0, 1.0, 4.0, 7.0, 10.0, 13.0],100,4000)
